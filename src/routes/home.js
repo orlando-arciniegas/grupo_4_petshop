da64861a3d941fffed49f05a670eef3ddfeb9126
orlando arciniegas <orlandoarciniegas@gmail.com>
2021-02-07 14:50:50 -0300

    Update form register & login

--- src/routes/home.js
+++ src/routes/home.js
@@ -6,6 +6,13 @@ const router = express.Router();
 router.route('/')
     .get((req, res) => res.sendFile(path.resolve(__dirname, '../views/home.html')))
 
+router.route('/register')
+    .get((req, res) => res.sendFile(path.resolve(__dirname, '../views/register.html')))
+
+router.route('/login')
+    .get((req, res) => res.sendFile(path.resolve(__dirname, '../views/login.html')))
+
 
 module.exports = router
+
 
