c2f3324a0c7ca2057e40b655d1add6dea7bc6fb8
Michael Bonilla <michaelbonilla13@hotmail.com>
2021-02-18 20:32:45 -0500

    Creacion de carpetas usuarios y productos en views

--- src/routes/home.js
+++ src/routes/home.js
@@ -7,16 +7,15 @@ router.route('/')
     .get((req, res) => res.render(path.resolve(__dirname, '../views/home.ejs')))
 
 router.route('/register')
-    .get((req, res) => res.sendFile(path.resolve(__dirname, '../views/register.html')))
+    .get((req, res) => res.render(path.resolve(__dirname, '../views/usuarios/register.ejs')))
 
 router.route('/login')
-    .get((req, res) => res.sendFile(path.resolve(__dirname, '../views/login.html')))
+    .get((req, res) => res.render(path.resolve(__dirname, '../views/usuarios/login.ejs')))
 
 router.route('/detalle')
-    .get((req, res) => res.sendFile(path.resolve(__dirname, '../views/detalle.html')))
+    .get((req, res) => res.render(path.resolve(__dirname, '../views/productos/detalle.ejs')))
 
-router.route('/carrito')
-    .get((req, res) => res.sendFile(path.resolve(__dirname, '../views/carrito.html')))
+    .get((req, res) => res.render(path.resolve(__dirname, '../views/productos/carrito.ejs')))
 
 
 module.exports = router
