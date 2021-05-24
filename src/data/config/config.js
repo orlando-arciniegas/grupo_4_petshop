ce7cb080ebb0833f317e64a7c3d0a77449dd3ae3
orlando arciniegas <orlandoarciniegas@gmail.com>
2021-05-24 12:28:40 -0300

    Update

--- src/data/config/config.js
+++ src/data/config/config.js
@@ -14,10 +14,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "petshop",
-    "host": "127.0.0.1",
+    "username": "ddka91ezee7eiuvl",
+    "password": 'n0lfjzw4y6qtb0yk',
+    "database": "afb6ucbjd5lh1ycx",
+    "host": "tvcpw8tpu4jvgnnq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     "dialect": "mysql"
   }
 }
